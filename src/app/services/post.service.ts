5a898ea6a40700453c6eed24de79081ce612cdd6
Carlo Meyer <ctmcarlo@gmail.com>
2022-03-01 15:41:05 +0200

    update

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 export interface Post {
   id: number;
